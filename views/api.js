0dd119892886c72577aa28a29f680235d1763066
noredinbah <lhrinourdine9@gmail.com>
2025-06-09 20:27:36 +0100

    api key

--- views/api.js
+++ views/api.js
@@ -1,2 +1,2 @@
-const API_KEY = "AIzaSyCO_qIPFiCwQf4Ph71v2YVgpqhdD_SfmQ0";
+const API_KEY = "ghjk";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
