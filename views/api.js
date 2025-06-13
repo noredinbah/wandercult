5281cc09296fa730778c0a480e4c9819980d4cd5
noredinbah <150736966+noredinbah@users.noreply.github.com>
2025-06-13 18:07:35 +0100

    Update api.js

--- views/api.js
+++ views/api.js
@@ -3,3 +3,6 @@ const API_KEY = "ghjk";
 
 /* istanbul ignore next */
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
+
+console.log(API_KEY+API_URL);
